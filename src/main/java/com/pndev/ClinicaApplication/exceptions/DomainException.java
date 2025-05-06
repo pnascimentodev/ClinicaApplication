39978ddd50bbc638e3b16d3969b257d6f309d9d1
Priscila Nascimento <pnmelo2808@gmail.com>
2025-05-06 18:54:36 -0300

    Feat: Create UserService + Exception

---
+++ src/main/java/com/pndev/ClinicaApplication/exceptions/DomainException.java
@@ -0,0 +1,19 @@
+package com.pndev.ClinicaApplication.exceptions;
+
+public class DomainException extends RuntimeException {
+
+    public enum ErrorType{
+        USER_NOT_FOUND
+    }
+
+    private final ErrorType errorType;
+
+    public DomainException(ErrorType errorType, String message) {
+        super(message);
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
+}
---
+++ src/main/java/com/pndev/ClinicaApplication/service/UserService.java
@@ -0,0 +1,41 @@
+package com.pndev.ClinicaApplication.service;
+
+import com.pndev.ClinicaApplication.dto.user.UserRegisterDTO;
+import com.pndev.ClinicaApplication.exceptions.DomainException;
+import com.pndev.ClinicaApplication.model.User;
+import com.pndev.ClinicaApplication.repository.DoctorRepository;
+import com.pndev.ClinicaApplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User register(UserRegisterDTO dto) {
+        User user = new User();
+        user.setName(dto.getName());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setEmail(dto.getEmail());
+        user.setRole(dto.getRole());
+
+        return userRepository.save(user);
+    }
+
+    public User findByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new DomainException(
+                        DomainException.ErrorType.USER_NOT_FOUND,
+                        "User not found with email: " + email
+                ));
+    }
+}
